8f475abb472765fec0092cb46a93d89223ff32a8
Giovani Sanfer <143708066+sanfergio@users.noreply.github.com>
2025-10-22 17:15:29 -0300

    add navigation again

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import WhatsAppButton from './components/WhatsappButton.jsx';
 import HomeButton from './components/HomeButton.jsx';
 import { Routes, Route } from 'react-router-dom'; // 1. Importar
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   )
