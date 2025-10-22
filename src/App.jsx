ff218ab5ae146450403133dd6e86f1a436c07a67
Giovani Sanfer <143708066+sanfergio@users.noreply.github.com>
2025-10-22 17:16:51 -0300

    fix little beat

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,6 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </>
   )
